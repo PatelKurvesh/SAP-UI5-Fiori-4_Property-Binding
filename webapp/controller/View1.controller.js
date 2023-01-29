89f5adf9bc0957829417e0ab5f0ac55190f0bf12
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-29 13:05:41 +0530

    View and Controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-4_Property-Binding.controller.View1", {
+		
+		onInit : function(){
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("model/student.json");
+			this.getView().setModel(oModel);
+		}
 
 	});
 });
