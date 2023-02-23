f3583838c77ff332ea6c8020f851ab98c39075c9
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-23 17:44:50 +0530

    updated all file

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,9 +6,8 @@ sap.ui.define([
 	return Controller.extend("GithubSAP-UI5-Fiori-4_Property-Binding.controller.View1", {
 		
 		onInit : function(){
-			var oModel = new sap.ui.model.json.JSONModel();
-			oModel.loadData("model/student.json");
-			this.getView().setModel(oModel);
+			
+			this.getView().setModel();
 		}
 
 	});
